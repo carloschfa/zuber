9b6494e8bd4e614f1dc81217f72b59bb72bf947f
Carlos Antunes <carlsochfa@gmail.com>
2020-03-13 17:18:18 -0300

    Adding initial login code.

--- Zuber/Controller/LoginController.swift
+++ Zuber/Controller/LoginController.swift
@@ -12,13 +12,29 @@ class LoginController: UIViewController {
   
   // MARK: - Properties
   
+  private let titleLabel: UILabel = {
+    let label = UILabel()
+    label.text = "ZUBER"
+    label.font = UIFont(name: "Avenir-Light", size: 36)
+    label.textColor = UIColor(white: 1, alpha: 0.8)
+    return label
+  }()
+  
   // MARK: - Lifecycle
   
   override func viewDidLoad() {
     
     super.viewDidLoad()
     
-    view.backgroundColor = .red
+    view.backgroundColor = UIColor(red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
+    
+    view.addSubview(titleLabel)
+    titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor)
+    titleLabel.centerX(in: view)
+  }
+  
+  override var preferredStatusBarStyle: UIStatusBarStyle {
+    return .lightContent
   }
   
 }
