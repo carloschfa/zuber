a0465e19d7c8215f076514a2d65d282852c19dfd
Carlos Antunes <carloschfa@gmail.com>
2020-08-27 21:01:49 -0300

    Adding password textfield, refactoring functions from controller to extensions avoiding duplicated code.

--- Zuber/Controller/LoginController.swift
+++ Zuber/Controller/LoginController.swift
@@ -9,84 +9,70 @@
 import UIKit
 
 class LoginController: UIViewController {
-  
-  // MARK: - Properties
-  
-  private let titleLabel: UILabel = {
-    let label = UILabel()
-    label.text = "ZUBER"
-    label.font = UIFont(name: "Avenir-Light", size: 36)
-    label.textColor = UIColor(white: 1, alpha: 0.8)
-    return label
-  }()
-  
-  private lazy var emailContainerView: UIView = {
-    let view = UIView()
-    let imageView = UIImageView()
-    imageView.image = #imageLiteral(resourceName: "ic_mail_outline_white_2x")
-    imageView.alpha = 0.87
-    view.addSubview(imageView)
     
-    imageView.centerY(in: view)
-    imageView.anchor(left: view.leftAnchor, paddingLeft: 8, width: 24, height: 24)
+    // MARK: - Properties
     
-    view.addSubview(emailTextField)
-    emailTextField.centerY(in: view)
-    emailTextField.anchor(left: imageView.rightAnchor,
-                          bottom: view.bottomAnchor,
-                          right: view.rightAnchor,
-                          paddingLeft: 8,
-                          paddingBottom: 8)
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "ZUBER"
+        label.font = UIFont(name: "Avenir-Light", size: 36)
+        label.textColor = UIColor(white: 1, alpha: 0.8)
+        return label
+    }()
     
-    let separatorView = UIView()
-    separatorView.backgroundColor = .lightGray
-    view.addSubview(separatorView)
-    separatorView.anchor(left: view.leftAnchor,
-                         bottom: view.bottomAnchor,
-                         right: view.rightAnchor,
-                         paddingLeft: 8,
-                         height: 0.75)
+    private lazy var emailContainerView: UIView = {
+        return UIView().inputContainerView(image: #imageLiteral(resourceName: "ic_mail_outline_white_2x"), textfield: emailTextField)
+    }()
     
-    return view
-  }()
-  
-  private let emailTextField: UITextField = {
-    let textfield = UITextField()
+    private let emailTextField: UITextField = {
+        return UITextField().textfield(withPlaceholder: "Email",
+                                       isSecureTextEntry: false)
+    }()
     
-    textfield.borderStyle = .none
-    textfield.font = UIFont.systemFont(ofSize: 16)
-    textfield.textColor = .white
-    textfield.keyboardAppearance = .dark
-    textfield.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
+    private lazy var passwordContainerView: UIView = {
+        return UIView().inputContainerView(image: #imageLiteral(resourceName: "ic_lock_outline_white_2x"), textfield: passwordTextField)
+    }()
     
-    return textfield
-  }()
-  
-  // MARK: - Lifecycle
-  
-  override func viewDidLoad() {
+    private let passwordTextField: UITextField = {
+        return UITextField().textfield(withPlaceholder: "Password",
+                                       isSecureTextEntry: true)
+    }()
     
-    super.viewDidLoad()
+    // MARK: - Lifecycle
     
-    view.backgroundColor = UIColor(red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor(red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
+        
+        view.addSubview(titleLabel)
+        titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor)
+        titleLabel.centerX(in: view)
+        
+        view.addSubview(emailContainerView)
+        view.addSubview(passwordContainerView)
+        
+        emailContainerView.anchor(top: titleLabel.bottomAnchor,
+                                  left: view.leftAnchor,
+                                  right: view.rightAnchor,
+                                  paddingTop: 40,
+                                  paddingLeft: 15,
+                                  paddingRight: 15,
+                                  height: 50)
+        
+        passwordContainerView.anchor(top: emailContainerView.bottomAnchor,
+        left: view.leftAnchor,
+        right: view.rightAnchor,
+        paddingTop: 16,
+        paddingLeft: 15,
+        paddingRight: 15,
+        height: 50)
+        
+    }
     
-    view.addSubview(titleLabel)
-    titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor)
-    titleLabel.centerX(in: view)
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
     
-    view.addSubview(emailContainerView)
-    emailContainerView.anchor(top: titleLabel.bottomAnchor,
-                              left: view.leftAnchor,
-                              right: view.rightAnchor,
-                              paddingTop: 40,
-                              paddingLeft: 15,
-                              paddingRight: 15,
-                              height: 50)
-    
-  }
-  
-  override var preferredStatusBarStyle: UIStatusBarStyle {
-    return .lightContent
-  }
-  
 }
